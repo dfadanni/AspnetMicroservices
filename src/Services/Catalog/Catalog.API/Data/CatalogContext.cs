15bfbdbc8a7a422263db24032e786ac4a8a6da5a
Diego Fadanni <dfadanni>
2022-02-04 15:12:29 -0300

    Add Catalog context.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var dbSettings = configuration.GetSection("DatabasedSettings");
+            var client = new MongoClient(dbSettings.GetValue<string>("ConnnectionString"));
+            var db = client.GetDatabase(dbSettings.GetValue<string>("DatabaseName"));
+
+            Products = db.GetCollection<Product>(dbSettings.GetValue<string>("CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> mongoCollection)
+        {
+            bool existCollection = mongoCollection.Find(_ => true).Any();
+            if (!existCollection)
+            {
+                mongoCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new Product[]
+            {
+                new Product
+                {
+                    Id = "111",
+                    Name = "Asus",
+                    Category = "Computers",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "ImageFile.png",
+                    Price= 54.93M
+                }
+            };
+        }
+    }
+}
